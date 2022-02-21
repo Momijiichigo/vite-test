b34908cd4276540d43d3b93b28252388d81c8995
Momijiichigo <ichigomomiji436@gmail.com>
2022-02-20 21:24:30 -0800

    up

--- src/Component/index.tsx
+++ src/Component/index.tsx
@@ -1,9 +1,15 @@
 import Worker from './worker?worker'
 
-const worker = new Worker();
-
-worker.onmessage = async (e) => {
-  console.log(e);
+const OFF_SCREEN_AVAILABLE = import.meta.env.PROD && 'transferControlToOffscreen' in HTMLCanvasElement.prototype
+if(OFF_SCREEN_AVAILABLE){
+  const worker = new Worker();
   
+  worker.onmessage = async (e) => {
+    console.log(e);
+    
+  }
 }
+
+
+
 export const o = 3
